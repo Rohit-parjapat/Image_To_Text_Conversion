a33cab62b2a7eca2a977e80ea3b079704749167b
Rohit <rparjapati7377@gmail.com>
2023-04-02 08:22:14 +0530

    Completed

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const Tesseract = require("tesseract.js");
+
+const app = express();
+
+app.use(express.json());
+
+// Home Route "/"
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Server is running",
+  });
+});
+
+// Captcha to text route "/captchatotext"
+
+app.get("/captchatotext", (req, res) => {
+  Tesseract.recognize(
+    "https://i.ibb.co/jTKYQqP/Captcha-United.png", //  <= put your captcha image here
+    "eng", // <= Language
+    {
+      logger: (m) => console.log(m),
+    }
+  ).then(({ data: { text } }) => {
+    console.log(text); // <== Output text
+    res.status(200).json(text);
+  });
+});
+
+const PORT = 4000;
+app.listen(PORT, () => {
+  console.log(`Server is started on port ${PORT}`);
+});
